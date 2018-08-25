394d9cca6969143a95cc084dcc0e3f844bfb79ce
Maksim Yegorov <findmaksim@gmail.com>
2018-08-24 20:34:13 -0700

    theme, font

--- init.el
+++ init.el
@@ -13,6 +13,20 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; auto install spacemacs theme
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+;; aliases
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; make zsh default shell
+(defvar my-term-shell "/usr/bin/zsh")
+(defadvice ansi-term (before force-bash)
+  (interactive (list my-term-shell)))
+(ad-activate 'ansi-term)
+
 (use-package which-key
   :ensure t
   :init
@@ -36,20 +50,28 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes nil)
+ '(custom-enabled-themes (quote (spacemacs-dark)))
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(ido-create-new-buffer (quote always))
  '(ido-enable-flex-matching t)
  '(ido-mode (quote both) nil (ido))
  '(ido-use-filename-at-point (quote guess))
- '(package-selected-packages (quote (which-key use-package xclip))))
+ '(package-selected-packages (quote (spacemacs-theme which-key use-package xclip))))
 
+;; transparent background for terminal emulator
+(defun on-after-init ()
+  (unless (display-graphic-p (selected-frame))
+    (set-face-background 'default "unspecified-bg" (selected-frame))))
+(add-hook 'window-setup-hook 'on-after-init)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "SRC" :family "Hack")))))
 
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
@@ -63,10 +85,10 @@
 (setq sentence-end-double-space nil)
 (xclip-mode 1)
 
-;; show line numbers
-;; add space and solid line between line numbers and text
-(setq linum-format "%4d \u2502 ")
-(global-linum-mode t)
+;; ;; show line numbers
+;; ;; add space and solid line between line numbers and text
+;; (setq linum-format "%4d \u2502 ")
+;; (global-linum-mode t)
 
 ;; set transparency level
 ;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
@@ -83,3 +105,5 @@
 (global-set-key (kbd "<M-down>") 'enlarge-window)
 (global-set-key (kbd "<M-left>") 'shrink-window-horizontally)
 (global-set-key (kbd "<M-right>") 'enlarge-window-horizontally)
+;; launch terminal
+(global-set-key (kbd "<s-return>") 'ansi-term)
