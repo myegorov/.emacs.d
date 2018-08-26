b966d31a02e2ee7e1f9fd8f4c004e26dbfe39bbb
Maksim Yegorov <findmaksim@gmail.com>
2018-08-26 03:02:02 -0700

    using org mode to configure emacs

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
-;; no splash screen
-(setq inhibit-startup-message t)
-
 ;; add a larger package repository
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -13,33 +10,11 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; auto install spacemacs theme
-(unless (package-installed-p 'spacemacs-theme)
-  (package-refresh-contents)
-  (package-install 'spacemacs-theme))
+;; see also config under ./emacs-init.org
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "emacs-init.org" user-emacs-directory))
 
-;; aliases
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; make zsh default shell
-(defvar my-term-shell "/usr/bin/zsh")
-(defadvice ansi-term (before force-bash)
-  (interactive (list my-term-shell)))
-(ad-activate 'ansi-term)
-
-(use-package which-key
-  :ensure t
-  :init
-  (which-key-mode))
-
-;; hide menu bar in terminal
-(unless (display-graphic-p)
-  (menu-bar-mode -1))
-
-(when (window-system)
-  (scroll-bar-mode -1)
-  (tool-bar-mode -1)
-  (menu-bar-mode -1))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -60,50 +35,9 @@
  '(ido-use-filename-at-point (quote guess))
  '(package-selected-packages (quote (spacemacs-theme which-key use-package xclip))))
 
-;; transparent background for terminal emulator
-(defun on-after-init ()
-  (unless (display-graphic-p (selected-frame))
-    (set-face-background 'default "unspecified-bg" (selected-frame))))
-(add-hook 'window-setup-hook 'on-after-init)
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "SRC" :family "Hack")))))
-
-(setq make-backup-files nil) ; stop creating backup~ files
-(setq auto-save-default nil) ; stop creating #autosave# files
-
-(global-visual-line-mode t)
-
-;; share system clipboard with emacs
-(setq x-select-enable-clipboard t)
-
-;; in sentence movement, do not require that sentences be separated with double spaces
-(setq sentence-end-double-space nil)
-(xclip-mode 1)
-
-;; ;; show line numbers
-;; ;; add space and solid line between line numbers and text
-;; (setq linum-format "%4d \u2502 ")
-;; (global-linum-mode t)
-
-;; set transparency level
-;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
-;;(set-frame-parameter (selected-frame) 'alpha <both>)
-(set-frame-parameter (selected-frame) 'alpha '(95 50))
-(add-to-list 'default-frame-alist '(alpha 95 50))
-
-;; custom bindings
-;; switch window (C-x o)
-(global-set-key (kbd "M-o") 'other-window)
-(windmove-default-keybindings) ; S-<left|right|up|down> to switch windows
-;; resize window
-(global-set-key (kbd "<M-up>") 'shrink-window)
-(global-set-key (kbd "<M-down>") 'enlarge-window)
-(global-set-key (kbd "<M-left>") 'shrink-window-horizontally)
-(global-set-key (kbd "<M-right>") 'enlarge-window-horizontally)
-;; launch terminal
-(global-set-key (kbd "<s-return>") 'ansi-term)
